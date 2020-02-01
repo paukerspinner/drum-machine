26850683617890c373098831e177c82973de4d44
paukerspinner <xuancanhpham97@gmail.com>
2020-02-01 19:40:34 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './myStyle.css'
+import { notes } from './constants/pianoKeys';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playedNotes: ''
+    }
+  }
+
+  indexOf = (prop, value) => {
+    let result = -1;
+    for (let i = 0; i < notes.length; i++) {
+      if (notes[i][prop] === value) { result = i; }
+    }
+    return result;
+  }
+
+  convert = (fromProp, toProp, value) => {
+    var index = this.indexOf(fromProp, value);
+    return index !== -1 ? notes[index][toProp] : '';
+  }
+
+  activeNote = () => {}
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+    document.addEventListener('keyup', this.handlleKeyUp);
+    document.addEventListener('click', this.handleClick);
+  }
+
+  handleClick = event => {
+    let symbol = event.target.id;
+    let key = this.convert('symbol', 'key', symbol);
+    let audio = document.querySelector(`[data-key="${key}"`);
+    if (this.playSound(audio))
+      this.setState({
+        playedNotes: [this.state.playedNotes, symbol.replace('_', '#')].join(" ")
+      })
+  }
+
+  playSound = (audio) => {
+    if (audio && !isNaN(audio.duration)) {
+      audio.currentTime = 0;
+      setTimeout(() => audio.play(), 10);
+      return true;
+    }
+    return false;
+  }
+
+  handlleKeyUp = (event) => {
+    let key = String.fromCharCode(event.keyCode);
+    let symbol = this.convert('key', 'symbol', key);
+    let tag = document.getElementById(symbol);
+    if (tag) {
+      tag.className = tag.className.replace(/actived-note\s/g, '');
+    }
+  }
+
+  handleKeyDown = (event) => {
+    let key = String.fromCharCode(event.keyCode);
+    let symbol = this.convert('key', 'symbol', key);
+    let tag = document.getElementById(symbol);
+    if (tag) {
+      tag.className = "actived-note " + tag.className;
+      tag.click();
+    }
+  }
+
+  handleChange = event => {
+    var name = event.target.name;
+    var value = event.target.value;
+    this.setState({ [name] : value });
+  }
+
+  render() {
+    var audios = notes.map((note, index) => {
+      return (
+        <audio key={index} data-key={note.key} src={note.url} />
+      )
+    })
+
+    var noteTags = notes.map((note, index) => {
+      var classNote = /_/.test(note.symbol) ? 'black-note' : 'white-note';
+      return (
+        <span key={index} id={note.symbol} className={classNote} />
+      )
+    })
+    return (
+      <div className="container" id="piano">
+        { noteTags }
+        { audios }
+        <textarea id="historyNotes" value={this.state.playedNotes} readOnly/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/constants/pianoKeys.js
@@ -0,0 +1,155 @@
+export const keyArr = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C_', 'D_', 'F_', 'G_', 'A_']
+
+export const C = './piano-keys/high_c.mp3'; // đồ
+export const D = './piano-keys/high_d.mp3'; // rê
+export const E = './piano-keys/high_e.mp3'; // mi
+export const F = './piano-keys/high_f.mp3'; // fa
+export const G = './piano-keys/high_g.mp3'; // son
+export const A = './piano-keys/high_a.mp3'; // la
+export const B = './piano-keys/high_b.mp3'; // xi
+/*
+export const c; // đồ
+export const d; // rê
+export const e; // mi
+export const f; // fa
+export const g; // son
+export const a; // la
+export const b; // xi
+
+export const c_; // đồ
+export const d_; // rê
+export const f_; // fa
+export const g_; // son
+export const a_; // la
+
+export const C_;
+export const D_;
+export const F_;
+export const G_;
+export const A_;*/
+
+export const notes = 
+[
+	// Midle notes
+	{
+		symbol : 'c',
+		key : 'Q',
+		url : './piano-keys/middle_c.mp3'
+	},
+	{
+		symbol : 'c_',
+		key : '2',
+		url : './piano-keys/mid_c_sharp.mp3'
+	},
+	{
+		symbol : 'd',
+		key : 'W',
+		url : './piano-keys/middle_d.mp3'
+	},
+	{
+		symbol : 'd_',
+		key : '3',
+		url : './piano-keys/mid_d_sharp.mp3'
+	},
+	{
+		symbol : 'e',
+		key : 'E',
+		url : './piano-keys/middle_e.mp3'
+	},
+	{
+		symbol : 'f',
+		key : 'R',
+		url : './piano-keys/middle_f.mp3'
+	},
+	{
+		symbol : 'f_',
+		key : '5',
+		url : './piano-keys/mid_f_sharp.mp3'
+	},
+	{
+		symbol : 'g',
+		key : 'T',
+		url : './piano-keys/middle_g.mp3'
+	},
+	{
+		symbol : 'g_',
+		key : '6',
+		url : './piano-keys/mid_g_sharp.mp3'
+	},
+	{
+		symbol : 'a',
+		key : 'Y',
+		url : './piano-keys/middle_a.mp3'
+	},
+	{
+		symbol : 'a_',
+		key : '7',
+		url : './piano-keys/mid_a_sharp.mp3'
+	},
+	{
+		symbol : 'b',
+		key : 'U',
+		url : './piano-keys/middle_b.mp3'
+	},
+	// High notes
+	{
+		symbol : 'C',
+		key : 'I',
+		url : './piano-keys/high_c.mp3'
+	},
+	{
+		symbol : 'C_',
+		key : '9',
+		url : './piano-keys/high_c_sharp.mp3'
+	},
+	{
+		symbol : 'D',
+		key : 'O',
+		url : './piano-keys/high_d.mp3'
+	},
+	{
+		symbol : 'D_',
+		key : '0',
+		url : './piano-keys/high_d_sharp.mp3'
+	},
+	{
+		symbol : 'E',
+		key : 'P',
+		url : './piano-keys/high_e.mp3'
+	},
+	{
+		symbol : 'F',
+		key : 'Û',
+		url : './piano-keys/high_f.mp3'
+	},
+	{
+		symbol : 'F_',
+		key : '»',
+		url : './piano-keys/high_f_sharp.mp3'
+	},
+	{
+		symbol : 'G',
+		key : 'Ý',
+		url : './piano-keys/high_g.mp3'
+	},
+	{
+		symbol : 'G_',
+		key : '',
+		url : './piano-keys/high_g_sharp.mp3'
+	},
+	{
+		symbol : 'A',
+		key : 'Ü',
+		url : './piano-keys/high_a.mp3'
+	},
+	{
+		symbol : 'A_',
+		key : '',
+		url : './piano-keys/high_a_sharp.mp3'
+	},
+	{
+		symbol : 'B',
+		key : '$',
+		url : './piano-keys/high_b.mp3'
+	}
+]
